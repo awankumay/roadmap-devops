779a55d07f0b1d9df8596e199f530e1543e607e0
awankumay <andika.pris2@gmail.com>
2024-11-20 12:14:38 +0700

    Update

--- ServerPerformanceStats/server-stats.sh
+++ ServerPerformanceStats/server-stats.sh
@@ -2,31 +2,47 @@
 # Created By Andika - 2024
 
 # Total CPU usage
+echo "######################"
+echo "# System Uptime Info #"
+echo "######################"
+
+uptime
 
 cpu_usage_stats() {
-    echo "Show Detail CPU Usage"
+    echo "###################"
+    echo "# Total CPU Usage #"
+    echo "###################"
+    top -bn1 | grep "%Cpu(s):" | cut -d ',' -f 4 | awk '{print "Usage: " 100-$1 "%"}'
 }
 
 # Total memory usage (Free vs Used including percentage)
 
-memory_usage_stats(){
-    echo "Show Detail Memory Usage"
+memory_usage_stats() {
+    echo "######################"
+    echo "# Total Memory Usage #"
+    echo "######################"
 }
 
 # Total disk usage (Free vs Used including percentage)
 
-disk_usage_stats(){
-    echo "Show Detail Disk Usage"
+disk_usage_stats() {
+    echo "######################"
+    echo "# Total Memory Usage #"
+    echo "######################"
 }
 
 # Top 5 processes by CPU usage
 
-top_5_cpu_usage(){
-    echo "Show TOP 5 CPU Usage"
+top_5_cpu_usage() {
+    echo "#############"
+    echo "# Top 5 CPU #"
+    echo "#############"
 }
 
 # Top 5 processes by memory usage
 
-top_5_memory_usage(){
-    echo "Show TOP 5 Memory Usage"
+top_5_memory_usage() {
+    echo "################"
+    echo "# Top 5 Memory #"
+    echo "################"
 }
