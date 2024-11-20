64bc905821e08ccd360c06b02a9cd5df3e0e6ba3
awankumay <andika.pris2@gmail.com>
2024-11-20 11:39:44 +0700

    Update server-stats.sh

--- ServerPerformanceStats/server-stats.sh
+++ ServerPerformanceStats/server-stats.sh
@@ -4,29 +4,29 @@
 # Total CPU usage
 
 cpu_usage_stats() {
-
+    echo "Show Detail CPU Usage"
 }
 
 # Total memory usage (Free vs Used including percentage)
 
 memory_usage_stats(){
-
+    echo "Show Detail Memory Usage"
 }
 
 # Total disk usage (Free vs Used including percentage)
 
 disk_usage_stats(){
-
+    echo "Show Detail Disk Usage"
 }
 
 # Top 5 processes by CPU usage
 
 top_5_cpu_usage(){
-
+    echo "Show TOP 5 CPU Usage"
 }
 
 # Top 5 processes by memory usage
 
 top_5_memory_usage(){
-
+    echo "Show TOP 5 Memory Usage"
 }
