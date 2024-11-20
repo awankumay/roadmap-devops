9fe14ad98ea92aa39b09c6d62b5445317a3d69bf
Andika Candra Pristiawan <andika.candra@lintasarta.co.id>
2024-11-20 11:09:41 +0700

    First Commit

---
+++ ServerPerformanceStats/server-stats.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Created By Andika - 2024
+
+# Total CPU usage
+
+cpu_usage_stats() {
+
+}
+
+# Total memory usage (Free vs Used including percentage)
+
+memory_usage_stats(){
+
+}
+
+# Total disk usage (Free vs Used including percentage)
+
+disk_usage_stats(){
+
+}
+
+# Top 5 processes by CPU usage
+
+top_5_cpu_usage(){
+
+}
+
+# Top 5 processes by memory usage
+
+top_5_memory_usage(){
+
+}
